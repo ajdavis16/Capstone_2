7a39cf668a402097cb1146fe9a934f351f274a2e
ajdavis16@hotmail.com <ajdavis16@hotmail.com>
2017-10-05 04:54:37 -0400

    added pic

---
+++ gameoflife/src/config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyAnj1NYcIsRSoGBAQvSMRShiljC-w82_gA",
+    authDomain: "game-of-life-bf478.firebaseapp.com",
+    databaseURL: "https://game-of-life-bf478.firebaseio.com",
+    projectId: "game-of-life-bf478",
+    storageBucket: "game-of-life-bf478.appspot.com",
+    messagingSenderId: "252045720006"
+  };
